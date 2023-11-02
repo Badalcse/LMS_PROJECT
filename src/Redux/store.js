342026ab60316f54f67b0e7c40d3df376597ea16
Badal kumar <118023518+Badalcse@users.noreply.github.com>
2023-11-02 08:43:36 +0530

    Add files via upload

---
+++ src/Redux/store.js
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import authReducer from './slices/authSlice';
+import courseReducer from './slices/courseSlice';
+import razorpayReducer from './slices/razorPaySlice';
+
+
+const store = configureStore({
+    reducer: {
+        auth: authReducer,
+        course: courseReducer,
+        razorpay: razorpayReducer
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false}),
+    devTools: true
+});
+export default store;
